4639a84091b30fb91de6e88774131b13e5edbc67
Joao Pedro Volante <joaopedrovolante@gmail.com>
2024-09-24 15:45:35 -0300

    test(lamps): add test for initial state

---
+++ src/test/java/test/LampsTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.example.Lamps;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LampsTest {
+
+    @Test
+    public void testInitial_0_0_Final_1_1() {
+        assertEquals(1, Lamps.minSwitches(0, 0, 1, 1));
+    }
+
+    @Test
+    public void testInitial_0_0_Final_0_1() {
+        assertEquals(2, Lamps.minSwitches(0, 0, 0, 1));
+    }
+
+    @Test
+    public void testInitial_1_0_Final_1_1() {
+        assertEquals(1, Lamps.minSwitches(1, 0, 1, 1));
+    }
+
+    @Test
+    public void testInitial_1_1_Final_0_1() {
+        assertEquals(1, Lamps.minSwitches(1, 1, 0, 1));
+    }
+}
